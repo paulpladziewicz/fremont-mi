cadc1261210ea8fd757bfa648b8eb577d201ee0c
Paul Pladziewicz <ppladziewicz@gmail.com>
2021-04-20 13:01:44 -0400

    initial commit following Tailwind install and home page setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="mx-4">
+        <h1 class="text-6xl font-bold mt-4">FremontMI</h1>
+        <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque autem consectetur
+            eaque, id magni maiores
+            nobis odio quaerat, quos recusandae rem sint suscipit vel, voluptatem. Aliquam architecto aspernatur,
+            consectetur
+            dolor eveniet, id nam nobis odit omnis possimus repudiandae veniam?</p>
+        <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque autem consectetur
+            eaque, id magni maiores
+            nobis odio quaerat, quos recusandae rem sint suscipit vel, voluptatem. Aliquam architecto aspernatur,
+            consectetur
+            dolor eveniet, id nam nobis odit omnis possimus repudiandae veniam?</p>
+    </section>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>FremontMI</title>
+</head>
+<body class="max-w-screen-xl mx-auto">
+
+@yield('content')
+</body>
+</html>
