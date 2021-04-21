9891157b54799f08fd00b2335af2739d58e24a99
Paul Pladziewicz <ppladziewicz@gmail.com>
2021-04-21 06:49:26 -0400

    Posts page functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function index()
     {
+        dd(auth()->user()->posts);
         return view('dashboard');
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        return view('posts.index');
+    public function index()
+    {
+        $posts = Post::paginate(5);
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
-    public function store(){
-        dd('okay');
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
